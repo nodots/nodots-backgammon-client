5f5079b18c5e6dd8cb01c67586c92c152f4e976e
Ken Riley <kenr@nodots.com>
2023-09-13 14:52:27 -0600

    Style tweaks;

--- src/Components/Board/index.tsx
+++ src/Components/Board/index.tsx
@@ -1,4 +1,4 @@
-import { Paper, Box } from '@mui/material'
+import { Paper, Box, Container } from '@mui/material'
 import { useState } from 'react'
 // Hooks
 import { useGame } from '../../game/useGame'
@@ -139,8 +139,8 @@ const Board = () => {
     const swQuadrant = board.quadrants.find(q => q.location === QuadrantLocation.SW) as QuadrantType
     const seQuadrant = board.quadrants.find(q => q.location === QuadrantLocation.SE) as QuadrantType
     return (
-      <div className='board'>
-        <Paper className='board-half' elevation={4}>
+      <Box className='board'>
+        <Paper className='board-half west' elevation={0}>
           <PointLabelContainer quadrant={nwQuadrant} startingPosition={13} quadrantLocation={QuadrantLocation.NW} />
           <Quadrant quadrant={nwQuadrant} startingPosition={13} quadrantLocation={QuadrantLocation.NW} />
           <Box className='roll-surface'>
@@ -149,11 +149,11 @@ const Board = () => {
           <Quadrant quadrant={swQuadrant} startingPosition={7} quadrantLocation={QuadrantLocation.SW} />
           <PointLabelContainer quadrant={swQuadrant} startingPosition={7} quadrantLocation={QuadrantLocation.SW} />
         </Paper>
-        <Box className='rail'>
+        <Paper className='rail'>
           <Paper className='rail-checker-box white'><Rail rail={game.board.rail.black} /></Paper>
           <Paper className='rail-checker-box black'><Rail rail={game.board.rail.white} /></Paper>
-        </Box>
-        <Paper className='board-half' elevation={4}>
+        </Paper>
+        <Paper className='board-half east' elevation={0}>
           <PointLabelContainer quadrant={neQuadrant} startingPosition={19} quadrantLocation={QuadrantLocation.NE} />
           <Quadrant quadrant={neQuadrant} startingPosition={19} quadrantLocation={QuadrantLocation.NE} />
           <Box className='roll-surface'>
@@ -162,16 +162,38 @@ const Board = () => {
           <Quadrant quadrant={seQuadrant} startingPosition={1} quadrantLocation={QuadrantLocation.SW} />
           <PointLabelContainer quadrant={seQuadrant} startingPosition={1} quadrantLocation={QuadrantLocation.SW} />
         </Paper>
-        <Box className='off-container'>
-          <Paper className='cube-container'></Paper>
-          <Paper className='dice-container'></Paper>
-          <Paper className='off-checker-box black'><Off off={game.board.off.black} /></Paper>
-          <Paper className='cube-container'><Cube /></Paper>
-          <Paper className='off-checker-box white'><Off off={game.board.off.white} /></Paper>
-          <Paper className='dice-container'></Paper>
-          <Paper className='cube-container'></Paper>
-        </Box>
-      </div>
+        <Paper className='off-container' elevation={0}>
+          <Box className='off-container-inner'>
+            <Paper className='cube-container'>
+              {game.cube.owner === 'black' && <Cube />}
+            </Paper>
+            <Paper className='dice-container black'>
+              {
+                game.players.white.active && <>
+                  <Die order={0} value={1} color='black' />
+                  <Die order={1} value={1} color='black' />
+                </>
+              }
+            </Paper>
+            <Paper className='off-checker-box black'><Off off={game.board.off.black} /></Paper>
+            <Paper className='cube-container'>
+              {game.cube.owner === undefined && <Cube />}
+            </Paper>
+            <Paper className='off-checker-box white'><Off off={game.board.off.white} /></Paper>
+            <Paper className='dice-container white'>
+              {
+                game.players.black.active && <>
+                  <Die order={0} value={1} color='white' />
+                  <Die order={1} value={1} color='white' />
+                </>
+              }
+            </Paper>
+            <Paper className='cube-container'>
+              {game.cube.owner === 'white' && <Cube />}
+            </Paper>
+          </Box>
+        </Paper>
+      </Box>
     )
 
   } else {
--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -3,10 +3,6 @@ import { Container, Paper, Box } from '@mui/material'
 import NavBar from '../../components/Core/NavBar'
 
 export const BoardPage = () => {
-  return <Box >
-    <Container className='board-frame'>
-      <Board />
-    </Container>
-  </Box>
+  return <Board />
 }
 
