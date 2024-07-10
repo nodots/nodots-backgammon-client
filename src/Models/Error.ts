029ce62de81c763250289711b9d86e5a969e36fa
Ken Riley <kenr@nodots.com>
2024-07-09 21:52:53 -0600

    WIP: Removed cruft post-rebase

--- src/Models/Error.ts
+++
@@ -1,9 +0,0 @@
-export type GameErrorType = 'Game' | 'Move' | 'Player' | 'Die' | 'Cube' | 'CheckerBox' | 'Quadrant' | 'Point'
-export class GameError extends Error {
-  model: GameErrorType
-  constructor ({ model, errorMessage }: { model?: GameErrorType; errorMessage: string }) {
-    super(errorMessage)
-    this.model = model || 'Game'
-  }
-
-}
