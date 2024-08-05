8b3440252c2df027100d7c52c75c22b23696e205
Ken Riley <kenr@nodots.com>
2024-08-05 15:41:03 -0600

    Cruft removal

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { Auth0Provider } from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(
-  // <Auth0Provider
-  //   domain="dev-8ykjldydiqcf2hqu.us.auth0.com"
-  //   clientId="qdkexB56guy3NFhWL3hH1vqB2zqDMwtk"
-  //   authorizationParams={{
-  //     redirect_uri: 'http://127.0.0.1:5173/sign-in',
-  //   }}
-  // >
-  <App />
-  // </Auth0Provider>
-)
+root.render(<App />)
