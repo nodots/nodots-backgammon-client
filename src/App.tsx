5417aa1fa78a8ac41b3013d3178f683f44371849
Ken Riley <kenr@nodots.com>
2024-08-15 00:26:38 -0600

    UX Tweak: Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider, CssBaseline } from '@mui/material'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import appTheme from './theme/theme'
 import GamePage from './Pages/GamePage'
+import { FoodBank } from '@mui/icons-material'
 const App = () => {
   return (
     <ThemeProvider theme={appTheme}>
@@ -11,6 +12,15 @@ const App = () => {
           <Routes>
             <Route path="/" element={<GamePage />} />
           </Routes>
+          <footer>
+            <div>
+              Copyright &copy; {new Date().getFullYear()} Nodots Advisory Group,
+              LLC. All Rights Reserved.
+            </div>
+            <div>
+              <a href="mailto:backgammon@nodots.com">backgammon@nodots.com</a>
+            </div>
+          </footer>
         </div>
       </Router>
     </ThemeProvider>
